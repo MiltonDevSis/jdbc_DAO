ababd5a5b14c549877ab45e4685096da22bd2e02
MiltonDevSis <49815038+MiltonDevSis@users.noreply.github.com>
2020-10-11 11:17:41 -0300

    initial commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,17 @@
 package application;
 
+import model.entities.Department;
+import model.entities.Seller;
+import java.util.Date;
+
 public class Program {
 
     public static void main(String[] args) {
 
+        Department department = new Department(1, "milton");
+        Seller seller = new Seller(2, "milton", "milton@gmail.com", new Date(), 5000.00, department);
+
+        System.out.println(seller);
 
     }
 }
